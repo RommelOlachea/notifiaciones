cd05f63647c50562a9cf50cdc9f0c4212977a275
Rommel Olachea Cota <97263258+RommelOlachea@users.noreply.github.com>
2023-02-28 15:55:17 -0700

    Inicio

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:notificaciones/screens/home_screen.dart';
+import 'package:notificaciones/screens/message_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: 'home',
+      routes: {'home': (_) => HomeScreen(), 'message': (_) => MessageScreen()},
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
